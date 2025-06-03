42212493939e84c1f045d8a9f7568ec2ac2ebe87
kapooyah776 <kapooyah776@gmail.com>
2025-06-04 02:17:52 +0700

    tes

--- Laravel/resources/views/backend/partials/message.blade.php
+++
@@ -1,5 +0,0 @@
-@if(session()->has('msg'))
-    <div class="alert alert-{{session('type')}}">
-        {!! session('msg') !!}
-    </div>
-@endif
--- Laravel/resources/views/frontend/frontend-home.blade.php
+++
@@ -1,4 +0,0 @@
-@extends('frontend.frontend-master')
-@section('content')
-@include('frontend.home-pages.home-'.get_static_option('home_page_variant'))
-@endsection
